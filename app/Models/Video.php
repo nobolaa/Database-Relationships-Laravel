541043555d3d073fdd95f210d3abc996e521dff2
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-23 01:07:13 +0200

    Added many to many polimorphic relationship between entities:     - Video/Post <-> Tag

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -16,4 +16,8 @@ class Video extends Model
     public function comments(){
         return $this->morphMany(Comment::class, "commentable");
     }
+    
+    public function tags(){
+        return $this->morphToMany(Tag::class, "taggable");
+    }
 }
