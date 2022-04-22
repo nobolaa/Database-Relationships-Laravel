4cee4ec106b7be2506830dbb188c87a4760ee88f
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-22 21:46:12 +0200

    Created 1 to 1 relation between User and Profile entities

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile(){
+        return $this->hasOne(Profile::class);
+        //hasOne(modeloTabla, nombreClaveForanea (user_id - tabla profile), nombreClave (id - tabla user))
+    }
 }
